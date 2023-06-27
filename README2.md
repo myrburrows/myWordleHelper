c8f112a58a3023247dcd72bcf90bbfc24f217373
Roger Burrows <90137418+myrburrows@users.noreply.github.com>
2023-06-27 13:24:17 -0500

    initialcommit

---
+++ README2.md
@@ -0,0 +1,20 @@
+# Wordle Helper
+Wordle is a simple word game created by a Software Engineer Wardle. I made this simple helper app to show remaining words in each step.
+
+Demo: https://anwarulislam.github.io/wordle-helper/
+
+I play Wordle game everyday. It's really fun and simple. We can play only once in a day and we do not share the actual answer rather some square sign to show our result. This is something interesting about this game. That's why it became a social game now.
+
+When I play, I try to start with some known and easy word where we have maximum number of vowel. After trying three or four times I easily can guess the actual word. But, sometimes I can't remember words to try. That's why I thought it's better to create a simple solution to show remaining words in each step. And I created this solution using `JavaScript` and `VueJS`
+
+## How it works?
+
+It's actually very straightforward. When we write a word in Wordle and submit the word we see three state of characters of that word. It can be *PRESENT*, *ABSENT* or *CORRECT*. Wordle hightlight those state with color. Correct is Green, Present is Orange, and Absent is Gray. After trying in wordle if you want to see all remaining words in *Wordle Helper* you have to write the word and you can change the state of characters of that word by taping on a single character. And after that you have to click on *Next* button to write next word. After clicking on a next button you will see a *Show* button to show remaining words of that step in a modal.
+
+<a><img src="https://user-images.githubusercontent.com/26295990/152162355-ead50a50-202a-49a8-9188-bc8c3384fa8c.png" align="left" height="auto" width="320" alt="Wordle Helper - Anwarul Islam"></a>
+<a><img src="https://user-images.githubusercontent.com/26295990/152162427-a69258e7-b464-4030-8d05-522ec52b7dfb.png" align="center" height="auto" width="320" alt="Wordle Helper - Anwarul Islam"></a>
+
+
+## Contribution
+
+If you think there is a bug or something you can optimize feel free to make a pull request. And if you love the script give it a star 😉
---
+++ main.js
@@ -0,0 +1,140 @@
+Vue.component("modal", {
+    template: "#modal-template"
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        showStep: null,
+        words: allWords || [],
+        boardState: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ],
+        evaluations: [
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+            ['absent', 'absent', 'absent', 'absent', 'absent'],
+        ],
+        keyboard: [
+            ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+            ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+            ['↵', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '←'],
+        ],
+        remainingWords: {},
+        currentIndex: 0
+    },
+    methods: {
+        setEvaluation: function (wordIndex, letterIndex) {
+            if (this.currentIndex !== wordIndex) return;
+            Vue.set(this.evaluations[wordIndex], letterIndex, this.nextEvValue(this.evaluations[wordIndex][letterIndex]));
+        },
+        nextEvValue: function (current) {
+            if (current == 'absent') {
+                return 'present';
+            } else if (current == 'present') {
+                return 'correct';
+            } else {
+                return 'absent';
+            }
+        },
+        writeWord: function (e) {
+            let word = this.boardState[this.currentIndex]
+
+            if (e.key.length == 1) {
+                if (word.length < 5) {
+                    word += e.key
+                    Vue.set(this.boardState, this.currentIndex, word);
+                }
+            }
+
+            // backspace
+            if (e.key == 'Backspace') {
+                if (word.length > 0) {
+                    word = word.substring(0, word.length - 1);
+                    Vue.set(this.boardState, this.currentIndex, word);
+                }
+            }
+
+            // Enter
+            if (e.key == 'Enter') {
+                this.filterWords();
+            }
+        },
+        pressKey: function (key) {
+            let event = { key }
+            if (key == '↵') {
+                event.key = 'Enter';
+            }
+            if (key == '←') {
+                event.key = 'Backspace';
+            }
+            this.writeWord(event);
+        },
+        splitWord: function (word) {
+            let arr = word.split('');
+            let remainingLength = 5 - arr.length;
+            for (let i = 0; i < remainingLength; i++) {
+                arr.push('');
+            }
+            return arr;
+        },
+        filterWords: function () {
+            let word = this.boardState[this.currentIndex];
+            let evaluation = this.evaluations[this.currentIndex];
+            if (word.length < 5) return;
+
+            let remainingWords = this.words
+
+            for (let i = 0; i < word.length; i++) {
+                let char = word[i];
+                let ev = evaluation[i];
+
+                if (!this.isSkippable(word, char, i)) {
+                    remainingWords = (ev == 'correct') ? remainingWords.withCharAndIndex(char, i)
+                        : (ev == 'present') ? remainingWords = remainingWords.withCharactersButNotInIndex(char, i)
+                            : remainingWords.cancelCharacters(char)
+                }
+            }
+
+            this.words = remainingWords;
+            this.remainingWords[this.currentIndex] = remainingWords;
+            this.currentIndex++;
+        },
+        isSkippable: function (word, character, k) {
+            let evaluation = this.evaluations[this.currentIndex];
+            let index = [];
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] == character) {
+                    index.push(i);
+                }
+            }
+
+            if (index.length < 2) return false;
+            if (evaluation[k] != 'absent') return false;
+
+            let skippableIndex;
+            for (let i = 0; i < index.length; i++) {
+                if (evaluation[index[i]] === 'absent') {
+                    skippableIndex = index[i];
+                    break;
+                }
+            }
+            return skippableIndex == k;
+        },
+        showRemainingWords: function (i) {
+            console.log(this.remainingWords[i]);
+            this.showStep = `${i}`;
+        }
+    },
+    created: function () {
+        window.addEventListener('keyup', this.writeWord)
+    },
+})
